3c13309726e35c982ee100167d88f7d732813d36
Nkevin03 <knezosi6@gmail.com>
2021-11-01 19:40:44 +0100

    Commit pari o dispari

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-const frase = prompt('Inserisci una parola');
+// Palindroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
 
-palindromo(frase);
-
+const frase = prompt('Inserisci una parola')
+palindromo(frase)
 function palindromo() {
   let reverse = '';
   for(let i = frase.length - 1; i >= 0; i--){
@@ -14,4 +16,48 @@ function palindromo() {
     document.getElementById('palindromo').innerHTML = `la parola inserita '${frase}' non é palindroma`;
     console.log('La parola non é palindroma');
   }
+}
+
+
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+
+//scelte
+const scelta = prompt('scegli pari o dispari');
+const numero = parseInt(prompt('Inserisci un numero da 1 a 5'));
+
+const somma = casuale() + numero;
+const valore = events(somma);
+
+// Condizioni
+if (valore === scelta) {
+    document.getElementById("risultato").innerHTML = "hai vinto";
+    document.getElementById("somma").innerHTML = `La somma é di: ${somma}`;
+} else{
+    document.getElementById("risultato").innerHTML = "hai perso";
+    document.getElementById("somma").innerHTML = `La somma é di: ${somma}`;
+}
+
+// Random
+function casuale() {
+    let num;
+    num = Math.floor(Math.random() * 5) + 1;
+    console.log(num)
+    return num
+}
+
+// Somma
+function events(somma){
+    let risultato
+    if(somma % 2 == 0){
+        risultato = 'pari';
+    }else{
+        risultato = 'dispari';
+    }
+    return risultato
 }
